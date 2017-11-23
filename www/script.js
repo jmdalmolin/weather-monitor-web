8a2967f9a59439691d97307359e9864c2d48f3a4
Joao Marcos Dalmolin <joaomdalmolin@gmail.com>
2017-11-23 01:27:23 -0200

    first commit

--- www/script.js
+++ www/script.js
@@ -5,21 +5,23 @@ var tabela;
 $(document).ready(function () {
     tabela = $('tbody');
     getStationList();
-    setInterval(getDados, 5000);
+    setInterval(getDados, 1000);
     // setInterval(sendDados, 5000);
 });
 
 function getDados() {
-    var dados = {limit: 1};
+    var dados = {
+        limit: undefined
+    };
     setStateAnimation(true);
     $.ajax({
         url: "http://localhost:2324/weathermonitor/data",
         data: dados,
         success: function (data) {
-            console.log(data);
+            // console.log(data);
             if (data !== undefined) {
                 if (dadoAtual !== undefined) {
-                    if (data._id !== dadoAtual._id) {
+                    if (data.index >= dadoAtual.index) {
                         preencherTabela(data);
                         dadoAtual = data;
                     }
@@ -55,6 +57,56 @@ function getDados() {
 //     });
 // }
 
+function sendStation() {
+
+    var dados = {
+        name: $('#name').val(),
+        description: $('#description').val(),
+        latitude: $('#latitude').val(),
+        longitude: $('#longitude').val(),
+        thermometer: $('#thermometer').val(),
+        barometer: $('#barometer').val(),
+        hygrometer: $('#hygrometer').val(),
+        anemometer: $('#anemometer').val(),
+        windsock: $('#windsock').val(),
+        pluviometer: $('#pluviometer').val()
+    };
+
+    if (dados.name == ""){
+        alert("Preencha o nome");
+        return;
+    }
+    if (dados.description == ""){
+        alert("Preencha a descricao");
+        return;
+    }
+    if (dados.latitude == ""){
+        alert("Preencha o latitude");
+        return;
+    }
+    if (dados.longitude == ""){
+        alert("Preencha o longitude");
+        return;
+    }
+
+    setStateAnimation(true);
+    $.ajax({
+        url: "http://localhost:2324/weathermonitor/station",
+        // data: JSON.stringify(dados),
+        data: dados,
+        success: function (data) {
+            console.log(data);
+            $('#response').text('Hash: ' + data.hash);
+            setStateAnimation(false);
+        },
+        error: function () {
+            alert('Falha ao cadastrar a estacao');
+        },
+        dataType: 'json',
+        method: "POST"
+    });
+}
+
 function getStationList() {
     setStateAnimation(true);
     $.ajax({
@@ -62,33 +114,71 @@ function getStationList() {
         data: undefined,
         success: function (data) {
             if (data.warning === undefined) {
-                console.log(data);
+                // console.log(data);
                 stations = data;
             }
+            preencherTabelaEstacoes(data);
             setStateAnimation(false);
         },
         dataType: 'json'
     });
 }
 
-function preencherTabela(data) {
-    var name = undefined;
-    stations.forEach(function (t) {
-        if (t._id === data._station_id) {
-            name = t.name;
-        }
-    });
-
-    var linha = "<tr>" +
-        "<td>" + (++contagem) + "</td>" +
-        "<td>" + new Date(data.storage_date).toLocaleString() + "</td>" +
-        "<td>" + (name !== undefined ? name : "[Indefinido]") + "</td>" +
-        "<td>" + (data.temperature !== undefined ? (data.temperature + "°C") : "&nbsp;") + "</td>" +
-        "<td>" + (data.relative_humidity !== undefined ? (data.relative_humidity + "%") : "&nbsp;") + "</td>" +
-        "</tr>";
-    console.log(linha);
-    $('#thead').after(linha);
+function clearStation() {
+    $('#name').val('');
+    $('#description').val('');
+    $('#latitude').val('');
+    $('#longitude').val('');
+    $('#thermometer').val('');
+    $('#barometer').val('');
+    $('#hygrometer').val('');
+    $('#anemometer').val('');
+    $('#windsock').val('');
+    $('#pluviometer').val('');
+    $('#response').text('&nbsp;');
 }
+
+function preencherTabelaEstacoes(datas) {
+    console.log(datas);
+    datas.forEach(function (data) {
+        console.log(data);
+        var linha = "<tr>" +
+            "<td><input class='station_check' type='checkbox' data-id='" + data.id + "'></td>" +
+            "<td>" + (data.id !== undefined ? data.id : "[Indefinido]") + "</td>" +
+            "<td>" + (data.name !== undefined ? data.name : "[Indefinido]") + "</td>" +
+            "<td>" + (data.description !== undefined ? data.description : "&nbsp;") + "</td>" +
+            "<td>" + (data.latitude !== undefined ? (data.latitude) : "&nbsp;") + "</td>" +
+            "<td>" + (data.longitude !== undefined ? (data.longitude) : "&nbsp;") + "</td>" +
+            "</tr>";
+        $('#thead_s').after(linha);
+    });
+}
+
+function preencherTabela(datas) {
+    var name = undefined;
+    datas.forEach(function (data) {
+        stations.forEach(function (t) {
+            // console.log(t);
+            if (data.station == t.id) {
+                // console.log(t.name);
+                data.station_name = t.name;
+            }
+        });
+        var linha = "<tr>" +
+            "<td>" + (data.id !== undefined ? data.id : "[Indefinido]") + "</td>" +
+            "<td>" + new Date(data.storage_date).toLocaleString() + "</td>" +
+            "<td>" + (data.station_name !== undefined ? data.station_name : "[Indefinido]") + "</td>" +
+            "<td>" + (data.temperature !== undefined ? (data.temperature + "°C") : "&nbsp;") + "</td>" +
+            "<td>" + (data.atmospheric_pressure !== undefined ? (data.atmospheric_pressure + "ml") : "&nbsp;") + "</td>" +
+            "<td>" + (data.relative_humidity !== undefined ? (data.relative_humidity + "%") : "&nbsp;") + "</td>" +
+            "<td>" + (data.wind_speed !== undefined ? (data.wind_speed + "m/s") : "&nbsp;") + "</td>" +
+            "<td>" + (data.wind_direction !== undefined ? (data.wind_direction + "°") : "&nbsp;") + "</td>" +
+            "<td>" + (data.precipitation !== undefined ? (data.precipitation + "mL") : "&nbsp;") + "</td>" +
+            "</tr>";
+        $('#thead_d').after(linha);
+    });
+}
+
 
 function setStateAnimation(state) {
     if (state) {
